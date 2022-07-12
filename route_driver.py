b1d75543655a819ada79bae90486ee22db45c368
Steve Burns <steven.m.burns@intel.com>
2022-07-12 06:36:25 -0700

    Add pip requirements

--- route_driver.py
+++ route_driver.py
@@ -5,7 +5,7 @@ from itertools import product
 from route_env import OneLayer
 
 
-env = OneLayer(8, 8)
+env = OneLayer(8, 8, tgt=(3,3))
 
 env.reset()
 
@@ -13,7 +13,7 @@ q_table = np.zeros(((env.n*env.m), env.action_space.n))
 
 # Hyperparameters
 alpha = 0.1
-gamma = 0.6
+gamma = 1.0
 epsilon = 0.1
 
 training_steps = 100000
@@ -47,8 +47,11 @@ for iter in range(training_steps):
         print(f"Episode: {iter}")
         for i in range(env.n):
             for j in range(env.m):
-                idx = i * env.m + j
-                print(f'{env._actions[np.argmax(q_table[idx])]}', end='')
+                if i == env._target_location[0] and j == env._target_location[1]:
+                    print(' ', end='')
+                else:
+                    idx = env._build_idx(i, j)
+                    print(f'{env._actions[np.argmax(q_table[idx])]}', end='')
             print()
 
 
@@ -58,6 +61,9 @@ print("Training finished.\n")
 """Evaluate agent's performance after Q-learning"""
 
 for i, j in product(range(env.n), range(env.m)):
+    if i == env._target_location[0] and j == env._target_location[1]:
+        continue
+
     idx = env._build_idx(i, j)
     print(f'i,j: {i},{j}', end='')
     for action_idx, q in enumerate(q_table[idx]):
@@ -65,33 +71,33 @@ for i, j in product(range(env.n), range(env.m)):
     print(f' => {env._actions[np.argmax(q_table[idx])]}', end='')
     lb = min(q_table[idx][env._action_to_int['R']],q_table[idx][env._action_to_int['D']])
     ub = max(q_table[idx][env._action_to_int['U']],q_table[idx][env._action_to_int['L']])
-    print(f' {ub-lb:.6f} {(ub-lb)/lb*100:.2f}%', end='')
     print()
 
 for i in range(env.n):
     for j in range(env.m):
-        idx = env._build_idx(i, j)
-        print(f'{env._actions[np.argmax(q_table[idx])]}', end='')
+        if i == env._target_location[0] and j == env._target_location[1]:
+            print(' ', end='')
+        else:
+            idx = env._build_idx(i, j)
+            print(f'{env._actions[np.argmax(q_table[idx])]}', end='')
     print()
 
 total_epochs = 0
 episodes = 10000
 
 for _ in range(episodes):
-    state = env.reset()
+    _ = env.reset()
     epoch = 0
     
     done = False
     
-    agent = state['agent']
-    expected_epochs = agent[0] + agent[1]
+    expected_epochs = env.dist_to_target()
 
     #print( f'starting at: {agent} expected_epochs: {expected_epochs}')
 
     while not done:
         action = np.argmax(q_table[env._get_agent_idx()])
-        state, reward, done, _ = env.step(action)
-        #print( f'epoch: {epoch} state: {state} reward: {reward} done: {done}')
+        _, reward, done, _ = env.step(action)
         epoch += 1
 
     assert expected_epochs == 0 or expected_epochs == epoch, (expected_epochs, epoch)
--- route_env.py
+++ route_env.py
@@ -8,7 +8,7 @@ from gym import Env, spaces
 class OneLayer(Env):
 
 
-    def __init__(self, n, m):
+    def __init__(self, n, m, tgt=(0,0)):
         self.n = n
         self.m = m
 
@@ -30,8 +30,8 @@ class OneLayer(Env):
                            'D': np.array((1, 0), dtype=np.int8),
                            'L': np.array((0, -1), dtype=np.int8)}
         
-        self._agent_location = np.array((self.n-1, self.m-1), dtype=np.int8)
-        self._target_location = np.array((0, 0), dtype=np.int8)
+        self.set_agent_location((self.n-1, self.m-1))
+        self.set_target_location(tgt)
 
 
 
@@ -51,13 +51,18 @@ class OneLayer(Env):
     def reset(self, seed=None, return_info=False, options=None):
         super().reset(seed=seed)
 
-        self._agent_location = np.array((self.np_random.randint(self.n), self.np_random.randint(self.m)), dtype=np.int8)
+        while True:
+            self.set_agent_location((self.np_random.randint(self.n), self.np_random.randint(self.m)))
+            if self.dist_to_target() > 0:
+                break
 
         observation = self._get_obs()
         info = self._get_info()
         return (observation, info) if return_info else observation
 
 
+    def dist_to_target(self):
+        return np.sum(np.abs(self._agent_location-self._target_location))
 
     def step(self, action):
         agent_location = self._agent_location
@@ -75,7 +80,7 @@ class OneLayer(Env):
         if j == 0 and dj == -1 or j == self.m-1 and dj == 1:
             jj = j
 
-        self._agent_location = np.array((ii, jj), dtype=np.int8) 
+        self.set_agent_location((ii, jj))
         done = (new_agent_location == self._target_location).all()
         reward = 0 if done else -1
 
@@ -84,12 +89,18 @@ class OneLayer(Env):
         return self._get_obs(), reward, done, self._get_info
         
 
+    def set_agent_location(self, p):
+        self._agent_location = np.array(p, dtype=np.int8)
+
+    def set_target_location(self, p):
+        self._target_location = np.array(p, dtype=np.int8)
+
 def test_A():
 
     env = OneLayer(4, 4)
 
     def t( state, action, new_state, new_reward, new_done):
-        env._agent_location = np.array(state, dtype=np.int8)
+        env.set_agent_location(state)
         next_state, reward, done, _ = env.step(env._action_to_int[action])
         assert (next_state['agent'] == new_state).all() and reward == new_reward and done == new_done
 
@@ -106,3 +117,6 @@ def test_A():
 def test_B():
     env = OneLayer(4, 4)
 
+    env.set_agent_location((3,3))
+    
+    assert env.dist_to_target() == 6
